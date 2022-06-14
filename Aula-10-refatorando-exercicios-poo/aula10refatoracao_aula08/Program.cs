4c0642502c5d8fd498cf738661e0fdaf5d0615ec
Hugo Gonzaga Diniz <99600747+Hugogdiniz@users.noreply.github.com>
2022-06-14 18:07:06 -0300

    Adicionando as atividades

---
+++ Aula-10-refatorando-exercicios-poo/aula10refatoracao_aula08/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace aula10refatoracao_aula08
+{
+
+
+    public class Filmes
+    {
+        public String[] listaDeFilmes = { "Clube dos Cinco", "A Hora do Pesadelo", "Karatê Kid", "Rocky IV", "Conan, o Bárbaro", "Highlander" };
+       
+        // 1.1. Apresenta todos os filmes da lista separados por ponto e vírgula.
+        public string apresentaTodosOsFilmesSeparadosPorPontoVirgula()
+        {
+            string lista = "";
+            for (int i = 0; i < listaDeFilmes.Length; i++)
+            {
+                lista += ($"{listaDeFilmes[i]}; ");
+            }
+            return lista;
+        }
+
+        //1.2. Apresentar quantidade de filmes da lista.
+        public int quantidadeTotalDeFilmes()
+        {
+            return listaDeFilmes.Length;
+        }
+
+        // 1.3. Buscar o nome do filme de acordo com seu índice.
+
+        public string buscarNomeDoFilmePeloIndice(int indiceDoFilme)
+        {
+            return listaDeFilmes[indiceDoFilme];
+        }
+
+        // 1.4. Adiciona um novo filme a lista pelo seu nome.
+        public String[] adicionaUmNovoFilmePeloSeuNome(string nomeDoFilme)
+        {
+            listaDeFilmes[listaDeFilmes.Length - 1] = nomeDoFilme;
+            return listaDeFilmes;
+        }
+
+        // 1.5. Atualiza um filme a partir do seu índice na lista.
+        public string[] atualizarUmFilmePeloSeuIndice(int indiceDoFilme, string filmeSubstituto)
+        {
+            listaDeFilmes[indiceDoFilme] = filmeSubstituto;
+            return listaDeFilmes;
+        }
+
+        // 1.6. Listar todos os filmes com seu índice/posição na lista.
+        public String listarTodosOsFilmesComSeuIndiceNaLista()
+        {
+            string lista = "";
+            for (int i = 0; i < listaDeFilmes.Length; i++)
+            {
+                int posicao = i + 1;
+                lista += ($"    indice:{i} - Posição:{posicao} - conteudo:{listaDeFilmes[i]}; \r\n");
+            }
+            return lista;
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Filmes listaDeFilmes01 = new Filmes();
+
+
+
+            Console.WriteLine($"1.1. {listaDeFilmes01.apresentaTodosOsFilmesSeparadosPorPontoVirgula()}");
+            Console.WriteLine($"1.2. {listaDeFilmes01.quantidadeTotalDeFilmes()}");
+            Console.WriteLine($"1.3. {listaDeFilmes01.buscarNomeDoFilmePeloIndice(2)}");
+            Console.WriteLine($"1.4. {listaDeFilmes01.adicionaUmNovoFilmePeloSeuNome("Batman")}");
+            Console.WriteLine($"1.5. {listaDeFilmes01.atualizarUmFilmePeloSeuIndice(1, "NovoFilme")}");
+            Console.WriteLine($"1.5. {listaDeFilmes01.apresentaTodosOsFilmesSeparadosPorPontoVirgula()}");
+            Console.WriteLine($"1.6.{listaDeFilmes01.listarTodosOsFilmesComSeuIndiceNaLista()}");
+
+        }
+    }
+}
+
+
+
+
---
+++ Aula-10-refatorando-exercicios-poo/aula10refatoracao_aula09/Program.cs
@@ -0,0 +1,397 @@
+﻿namespace aula10refatoracao_aula09
+{
+
+    public class calculadora
+    {
+        public double resultado = 0;
+
+        public double somarValor()
+        {
+            double valor;
+            double resultadoFuncao;
+            Console.WriteLine("Você digitou 1 - Somar");
+            Console.WriteLine("Qual valor deseja Somar");
+            try
+            {
+                valor = double.Parse(Console.ReadLine());
+                resultadoFuncao = resultado + valor;
+                definirResultadoDaUltimaOperacaoMatematica(resultadoFuncao);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Você deve digitar um número -> " + ex.Message);
+            }
+
+
+
+
+
+
+
+            return pegarResultadoDaUltimaOperacaoMatematica();
+        }
+
+        public double subtrairValor()
+        {
+            double valor;
+            double resultadoFuncao;
+            Console.WriteLine("Você digitou 2 - Subtrair");
+            Console.WriteLine("Qual valor deseja Subtrair?");
+            try
+            {
+                valor = double.Parse(Console.ReadLine());
+                resultadoFuncao = resultado - valor;
+                definirResultadoDaUltimaOperacaoMatematica(resultadoFuncao);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Você deve digitar um número -> " + ex.Message);
+            }
+
+
+
+
+            return pegarResultadoDaUltimaOperacaoMatematica();
+        }
+
+        public void dividePorZero(double valor)
+        {
+            if (valor == 0)
+            {
+                throw new DivideByZeroException(message: "Não pode ser zero");
+            }
+        }
+
+        public double dividirValor()
+        {
+
+            double valor;
+            double resultadoFuncao;
+            Console.WriteLine("Você digitou 4 - Dividir");
+            Console.WriteLine("Qual valor deseja Dividir?");
+            try
+            {
+                valor = double.Parse(Console.ReadLine());
+                try
+                {
+                    dividePorZero(valor);
+                    resultadoFuncao = resultado / valor;
+                    definirResultadoDaUltimaOperacaoMatematica(resultadoFuncao);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Não pode dividir por zero");
+
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Você deve digitar um número -> " + ex.Message);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Não pode dividir por zero");
+
+            }
+            return pegarResultadoDaUltimaOperacaoMatematica();
+        }
+
+        public double multiplicarValor()
+        {
+            double valor;
+            double resultadoFuncao;
+            Console.WriteLine("Você digitou 3 - Multiplicar");
+            Console.WriteLine("Qual valor deseja Multiplicar?");
+            try
+            {
+                valor = double.Parse(Console.ReadLine());
+                resultadoFuncao = resultado * valor;
+                definirResultadoDaUltimaOperacaoMatematica(resultadoFuncao);
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Você deve digitar um número -> " + ex.Message);
+            }
+
+            return pegarResultadoDaUltimaOperacaoMatematica();
+        }
+        
+        public void zerarResultado()
+        {
+            definirResultadoDaUltimaOperacaoMatematica(0);
+        }
+
+        public double pegarResultadoDaUltimaOperacaoMatematica()
+        {
+
+            return resultado;
+        }
+
+        public void definirResultadoDaUltimaOperacaoMatematica(double resultadoFuncao)
+        {
+            resultado = resultadoFuncao;
+        }
+
+        public int menu()
+        {
+            int op;
+            Console.WriteLine("Menu de opções da calculadora");
+            Console.WriteLine("1 - Somar");
+            Console.WriteLine("2 - Subtrair");
+            Console.WriteLine("3 - Multiplicar");
+            Console.WriteLine("4 - Dividir");
+            Console.WriteLine("5 - Zerar Calculadora");
+            Console.WriteLine("0 - Terminar");
+            Console.WriteLine("Sua opção: ");
+            op = int.Parse(Console.ReadLine());
+
+            return op;
+        }
+        public int opcao;
+        public int continua = 1;
+        public double ultimoValor;
+        
+        
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            calculadora calculadora01 = new calculadora();
+
+            do
+            {
+                calculadora01.opcao = calculadora01.menu();
+                switch (calculadora01.opcao)
+                {
+                    case 0:
+                        calculadora01.continua = 0;
+                        break;
+                    case 1:
+
+                        Console.WriteLine(calculadora01.somarValor());
+                        break;
+                    case 2:
+                        Console.WriteLine(calculadora01.subtrairValor());
+                        break;
+                    case 3:
+
+                        Console.WriteLine(calculadora01.multiplicarValor());
+                        break;
+                    case 4:
+                        Console.WriteLine(calculadora01.dividirValor());
+                        break;
+                    case 5:
+                        calculadora01.zerarResultado();
+                        break;
+                    default:
+                        Console.WriteLine("Opção invalida! Por favor digite uma das opções acima.");
+                        break;
+                }
+
+            } while (calculadora01.continua != 0);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// double resultado = 0;
+
+
+
+//double somarValor()
+//{
+//    double valor;
+//    double resultadoFuncao;
+//    Console.WriteLine("Você digitou 1 - Somar");
+//    Console.WriteLine("Qual valor deseja Somar");
+//    try
+//    {
+//        valor = double.Parse(Console.ReadLine());
+//        resultadoFuncao = resultado + valor;
+//        definirResultadoDaUltimaOperacaoMatematica(resultadoFuncao);
+//    }
+//    catch (FormatException ex)
+//    {
+//        Console.WriteLine("Você deve digitar um número -> " + ex.Message);
+//    }
+
+
+
+
+
+
+
+//    return pegarResultadoDaUltimaOperacaoMatematica();
+//}
+
+//double subtrairValor()
+//{
+//    double valor;
+//    double resultadoFuncao;
+//    Console.WriteLine("Você digitou 2 - Subtrair");
+//    Console.WriteLine("Qual valor deseja Subtrair?");
+//    try
+//    {
+//        valor = double.Parse(Console.ReadLine());
+//        resultadoFuncao = resultado - valor;
+//        definirResultadoDaUltimaOperacaoMatematica(resultadoFuncao);
+//    }
+//    catch (FormatException ex)
+//    {
+//        Console.WriteLine("Você deve digitar um número -> " + ex.Message);
+//    }
+
+
+
+
+//    return pegarResultadoDaUltimaOperacaoMatematica();
+//}
+
+//void dividePorZero(double valor)
+//{
+//    if (valor == 0)
+//    {
+//        throw new DivideByZeroException(message: "Não pode ser zero");
+//    }
+//}
+
+//double dividirValor()
+//{
+
+//    double valor;
+//    double resultadoFuncao;
+//    Console.WriteLine("Você digitou 4 - Dividir");
+//    Console.WriteLine("Qual valor deseja Dividir?");
+//    try
+//    {
+//        valor = double.Parse(Console.ReadLine());
+//        try
+//        {
+//            dividePorZero(valor);
+//            resultadoFuncao = resultado / valor;
+//            definirResultadoDaUltimaOperacaoMatematica(resultadoFuncao);
+//        }
+//        catch (Exception ex)
+//        {
+//            Console.WriteLine("Não pode dividir por zero");
+
+//        }
+//    }
+//    catch (FormatException ex)
+//    {
+//        Console.WriteLine("Você deve digitar um número -> " + ex.Message);
+//    }
+
+//    catch (Exception ex)
+//    {
+//        Console.WriteLine("Não pode dividir por zero");
+
+//    }
+//    return pegarResultadoDaUltimaOperacaoMatematica();
+//}
+
+//double multiplicarValor()
+//{
+//    double valor;
+//    double resultadoFuncao;
+//    Console.WriteLine("Você digitou 3 - Multiplicar");
+//    Console.WriteLine("Qual valor deseja Multiplicar?");
+//    try
+//    {
+//        valor = double.Parse(Console.ReadLine());
+//        resultadoFuncao = resultado * valor;
+//        definirResultadoDaUltimaOperacaoMatematica(resultadoFuncao);
+//    }
+
+//    catch (FormatException ex)
+//    {
+//        Console.WriteLine("Você deve digitar um número -> " + ex.Message);
+//    }
+
+//    return pegarResultadoDaUltimaOperacaoMatematica();
+//}
+
+//void zerarResultado()
+//    {
+//        definirResultadoDaUltimaOperacaoMatematica(0);
+//    }
+
+//double pegarResultadoDaUltimaOperacaoMatematica()
+//{
+
+//    return resultado;
+//}
+
+//void definirResultadoDaUltimaOperacaoMatematica(double resultadoFuncao)
+//{
+//    resultado = resultadoFuncao;
+//}
+
+//int menu()
+//{
+//    int op;
+//    Console.WriteLine("Menu de opções da calculadora");
+//    Console.WriteLine("1 - Somar");
+//    Console.WriteLine("2 - Subtrair");
+//    Console.WriteLine("3 - Multiplicar");
+//    Console.WriteLine("4 - Dividir");
+//    Console.WriteLine("5 - Zerar Calculadora");
+//    Console.WriteLine("0 - Terminar");
+//    Console.WriteLine("Sua opção: ");
+//    op = int.Parse(Console.ReadLine());
+
+//    return op;
+//}
+//int opcao;
+//int continua = 1;
+//double ultimoValor;
+//do
+//{
+
+//    opcao = menu();
+//    switch (opcao)
+//    {
+//        case 0:
+//            continua = 0;
+//            break;
+//        case 1:
+
+//            Console.WriteLine(somarValor());
+//            break;
+//        case 2:
+//            Console.WriteLine(subtrairValor());
+//            break;
+//        case 3:
+
+//            Console.WriteLine(multiplicarValor());
+//            break;
+//        case 4:
+//            Console.WriteLine(dividirValor());
+//            break;
+//        case 5:
+//            zerarResultado();
+//            break;
+//        default:
+//            Console.WriteLine("Opção invalida! Por favor digite uma das opções acima.");
+//            break;
+//    }
+
+//} while (continua != 0);
